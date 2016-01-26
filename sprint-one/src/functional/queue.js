be7e01041b3dafe5223db8a017eb59e7a5ff6a56
Tritia Timmins <tritiatimmins@Tritias-MacBook-Pro.local>
2016-01-26 14:12:26 -0800

    makes changes to the queue. adds new variables that indicate the first and last values in the object

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,10 @@
 var Queue = function(){
   var count = 0;
+  //add lowest index for index 1
+  var lowestIndex = 0;
+  //add highestIndex for thing 2
+  var highestIndex = 0;
+  
   var someInstance = {};
 
   // Use an object with numeric keys to store values
@@ -9,14 +14,24 @@ var Queue = function(){
 
   someInstance.enqueue = function(value){
     //when an item is added the count changes
-    storage[count] = value;
-    count ++
+    //storage[count] = value;
+    storage[highestIndex] = value;
+    highestIndex++;
+    count ++;
   };
 
   someInstance.dequeue = function(){
+  //saving the first index of storage
+   var dequeued = storage[lowestIndex];
+   
+   delete storage[lowestIndex];
+   lowestIndex++;
+    
     if(count) {
     count --
     }
+    //return dequeued
+    return dequeued;
   };
 
   someInstance.size = function(){
