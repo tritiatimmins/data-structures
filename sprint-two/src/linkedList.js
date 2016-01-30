d2069c21ee4a47790818712efc927931f0e953e5
tritiatimmins <tritialtimmins@gmail.com>
2016-01-29 17:28:49 -0800

    finishes first test in linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,32 @@ var LinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
+   
 
   list.addToTail = function(value){
-  };
+    //create new node
+    //var newNode = Node(value);
+    list.head = Node(value);  
+      //change the next property of the current tail to the new node
+      list.tail = Node(value);
+      //create a variable for the current tail
+      //var currentTail = Node(value);
 
+      //set the current tail equal to the new node
+      //currentTail.next = newNode;
+      
+   };
+ 
   list.removeHead = function(){
+    
+    list.tail.next = 0;
+    console.log(list.tail.next);
+ 
   };
 
   list.contains = function(target){
+ 
+  
   };
 
   return list;
